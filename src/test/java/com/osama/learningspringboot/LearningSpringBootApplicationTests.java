b91bc54f82a8166b7ac8cc1cc206f0f8a337e25e
Osama Khan <52834187+gitosamakhan@users.noreply.github.com>
2021-01-18 12:54:36 +0500

    initial commit

---
+++ src/test/java/com/osama/learningspringboot/LearningSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osama.learningspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
