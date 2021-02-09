fdb584aa98f62f06e1a7ae0190df71724ae06cd5
Osama Khan <khannosa@Osamas-MacBook-Pro.local>
2021-02-09 18:19:00 +0500

    adding database connection

--- src/main/java/com/osama/learningspringboot/LearningSpringBootApplication.java
+++ src/main/java/com/osama/learningspringboot/LearningSpringBootApplication.java
@@ -3,10 +3,30 @@ package com.osama.learningspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 @SpringBootApplication
 public class LearningSpringBootApplication {
 
+
 	public static void main(String[] args) {
+
+		/*
+		 * Database: MySQL
+		 * Checking Database Connection */
+		try{
+			System.out.println("Connecting to the database...");
+			String jdbcUrl = "jdbc:mysql://localhost:3306/user?allowPublicKeyRetrieval=true&useSSL=false";
+			Connection connection = DriverManager.getConnection(jdbcUrl, "root", "osamakhan");
+			System.out.println("Connected to the Database!");
+		} catch(Exception exp) {
+			exp.printStackTrace();
+		}
+
+
+		/*
+		* Running the Application */
 		SpringApplication.run(LearningSpringBootApplication.class, args);
 	}
 
